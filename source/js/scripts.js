32168042ee234e3d43d11667daa31a7ace00442d
Евгений <msk9199625687@yandex.ru>
2022-04-18 15:20:04 +0300

    подключение JS

---
+++ source/js/scripts.js
@@ -0,0 +1,25 @@
+let navMainSite = document.querySelector('.site-list');
+let navMainUser = document.querySelector('.user-list');
+let navToggle = document.querySelector('.main-nav__toggle');
+
+navMainSite.classList.remove('main-nav__list--no-js');
+navMainUser.classList.remove('main-nav__list--no-js');
+
+
+navToggle.addEventListener('click', function () {
+  if (navMainSite.classList.contains('main-nav__list--closed')) {
+    navMainSite.classList.remove('main-nav__list--closed');
+    navMainSite.classList.add('main-nav__list--opened');
+  } else {
+    navMainSite.classList.add('main-nav__list--closed');
+    navMainSite.classList.remove('main-nav__list--opened');
+  }
+
+  if (navMainUser.classList.contains('main-nav__list--closed')) {
+    navMainUser.classList.remove('main-nav__list--closed');
+    navMainUser.classList.add('main-nav__list--opened');
+  } else {
+    navMainUser.classList.add('main-nav__list--closed');
+    navMainUser.classList.remove('main-nav__list--opened');
+  }
+});
